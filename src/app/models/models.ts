02abff28ebab629076f6918790a8909632695679
Alexey <alexeyushakov1997@yandex.ru>
2025-05-16 21:43:31 +0300

    feat: created Signup

--- src/app/models/models.ts
+++ src/app/models/models.ts
@@ -2,3 +2,12 @@ export interface SigninData {
     email: string;
     password: string;
 }
+
+export interface SignupData {
+    name: string;
+    nickname: string;
+    email: string;
+    sex: string;
+    password: string;
+    confirmPassword: string;
+}
